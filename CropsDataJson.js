315abca3b7e769c144abb783cb8ef3ede9b5fc15
ANSWERapp <avigailbenor@gmail.com>
2016-07-13 13:23:06 +0300

    Add files via upload

---
+++ CropsDataJson.js
@@ -0,0 +1,17 @@
+
+
+var cropsDataSet = [
+		
+		{ "id":1, "cropName": "Banana", "Tp":6, "EC50":2.2, "p":3, "PsiRoot":6000, "b":10, "Yr0":0},
+		{ "id":2, "cropName": "Corn", "Tp":4, "EC50":5, "p":3, "PsiRoot":6000, "b":10, "Yr0":0 },
+		{ "id":3, "cropName": "Pepper", "Tp":5, "EC50":2.5, "p":3, "PsiRoot":6000, "b":10,  "Yr0":0 }
+		
+		];
+		
+var soilsDataSet = [
+		
+		{"id":1, "soilName": "Clay", "Ks":225, "Beta":0.15, "Eta":2.2, "Theta_s":0.6, "Theta_r":0.1, "PsiWater":300 },
+		{"id":2, "soilName": "Sandy loam", "Ks":3600, "Beta":0.55, "Eta":2.7, "Theta_s":0.41, "Theta_r":0.06, "PsiWater":200 },
+		{"id":3, "soilName": "Silt loam", "Ks":600, "Beta":0.25, "Eta":2.5, "Theta_s":0.46, "Theta_r":0.05, "PsiWater":300 }
+		
+		];
