8dd0ed8e27a7e6377e7385ef1c4b2b3d127b3649
ANSWERapp <avigailbenor@gmail.com>
2016-08-01 11:02:07 +0300

    Add files via upload

---
+++ ANSWERappWeb_Hectare.js
@@ -0,0 +1,21 @@
+
+		
+function changeLabelsHectare(){
+			
+	$$("quotaPerDunam").define("label", "Quota per hectare:");
+	$$("quotaPerDunam").refresh();
+
+	$$("dunamAmount").define("label", "Hectare amount:");
+	$$("dunamAmount").refresh();
+
+	$$("returnPerDunam").define("label", "Return (per hectare):");
+	$$("returnPerDunam").refresh();
+
+	$$("fixedCostsPerDunam").define("label", "Fixed (per hectare):");
+	$$("fixedCostsPerDunam").refresh();
+
+
+}
+	
+		
+		
