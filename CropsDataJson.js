27caa239eadeaba669326cd4b12219641d468263
ANSWERapp <avigailbenor@gmail.com>
2016-08-08 11:06:22 +0300

    Update CropsDataJson.js

--- CropsDataJson.js
+++ CropsDataJson.js
@@ -2,18 +2,18 @@
 
 var cropsDataSet = [
 		
-		{ "id":1, "cropName": "Banana", "Tp":6, "EC50":2.2, "p":3, "PsiRoot":6000, "b":10, "Yr0":0, "Rev":1826, "Ret":1917, "YDP":190, "Fixed":4714, "Season":5, "y100":6.88},
-		{ "id":2, "cropName": "Basil", "Tp":4, "EC50":5, "p":3, "PsiRoot":6000, "b":10, "Yr0":0, "Rev":18200, "Ret":5100, "YDP":6286, "Fixed":49466, "Season":10, "y100":5.2318},
+		{ "id":1, "cropName": "Banana (shaded net)", "Tp":6, "EC50":2.2, "p":3, "PsiRoot":6000, "b":10, "Yr0":0, "Rev":1826, "Ret":1917, "YDP":190, "Fixed":4714, "Season":5, "y100":6.88},
+		{ "id":2, "cropName": "Basil (greenhouse/net-house)", "Tp":4, "EC50":5, "p":3, "PsiRoot":6000, "b":10, "Yr0":0, "Rev":18200, "Ret":5100, "YDP":6286, "Fixed":49466, "Season":10, "y100":5.2318},
 		{ "id":3, "cropName": "Corn", "Tp":4, "EC50":5, "p":3, "PsiRoot":6000, "b":10, "Yr0":0, "Rev":842, "Ret":131, "YDP":0, "Fixed":658, "Season":4, "y100":2}, 
 		{ "id":4, "cropName": "Dates", "Tp":5.5, "EC50":5, "p":3, "PsiRoot":6000, "b":10, "Yr0":0, "Rev":12940, "Ret":1191, "YDP":2455, "Fixed":5850, "Season":12, "y100":1.7104},
 		{ "id":5, "cropName": "Grape", "Tp":9, "EC50":2.5, "p":3, "PsiRoot":6000, "b":10, "Yr0":0, "Rev":7500, "Ret":1850, "YDP":1620, "Fixed":6849, "Season":7, "y100":2.51}, 
 		{ "id":6, "cropName": "Mango", "Tp":6, "EC50":3, "p":3, "PsiRoot":6000, "b":10, "Yr0":0, "Rev":2223, "Ret":515, "YDP":468, "Fixed":2044, "Season":5, "y100":3.83},
 		{ "id":7, "cropName": "Melon", "Tp":6, "EC50":7.5, "p":3, "PsiRoot":6000, "b":10, "Yr0":0, "Rev":3535, "Ret":693, "YDP":144, "Fixed":15177, "Season":3, "y100":5}, 
 		{ "id":8, "cropName": "Onion", "Tp":7, "EC50":3.5, "p":3, "PsiRoot":6000, "b":10, "Yr0":0, "Rev":1808, "Ret":522, "YDP":722, "Fixed":4638, "Season":8, "y100":6.5}, 
-		{ "id":9, "cropName": "Parsely", "Tp":4, "EC50":3.5, "p":3, "PsiRoot":6000, "b":10, "Yr0":0, "Rev":5000, "Ret":2800, "YDP":2313, "Fixed":4639, "Season":8, "y100":5.209},
-		{ "id":10, "cropName": "Pepper", "Tp":5, "EC50":2.5, "p":3, "PsiRoot":6000, "b":10,  "Yr0":0, "Rev":4833, "Ret":7979, "YDP":2080, "Fixed":19200, "Season":8, "y100":12.5}, 
+		{ "id":9, "cropName": "Parsely (greenhouse/net-house)", "Tp":4, "EC50":3.5, "p":3, "PsiRoot":6000, "b":10, "Yr0":0, "Rev":5000, "Ret":2800, "YDP":2313, "Fixed":4639, "Season":8, "y100":5.209},
+		{ "id":10, "cropName": "Pepper (greenhouse/net-house)", "Tp":5, "EC50":2.5, "p":3, "PsiRoot":6000, "b":10,  "Yr0":0, "Rev":4833, "Ret":7979, "YDP":2080, "Fixed":19200, "Season":8, "y100":12.5}, 
 		{ "id":11, "cropName": "Sunflower", "Tp":8, "EC50":10, "p":3, "PsiRoot":6000, "b":10, "Yr0":0, "Rev":6099, "Ret":147, "YDP":0, "Fixed":685, "Season":3, "y100":0.23}, 
-		{ "id":12, "cropName": "Tomato", "Tp":6, "EC50":5.5, "p":3, "PsiRoot":6000, "b":10, "Yr0":0, "Rev":3100, "Ret":6649, "YDP":1235, "Fixed":44135, "Season":10, "y100":30.8325}
+		{ "id":12, "cropName": "Tomato (greenhouse/net-house)", "Tp":6, "EC50":5.5, "p":3, "PsiRoot":6000, "b":10, "Yr0":0, "Rev":3100, "Ret":6649, "YDP":1235, "Fixed":44135, "Season":10, "y100":30.8325}
 		
 		];
 		
